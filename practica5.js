c3d1c7841b3cd35c238eec1cf7e2db1cb63635d4
mrMOY <srmoy82@gmail.com>
2022-08-16 09:31:05 -0600

    practica5

---
+++ practica5.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('canvas1');
+var ctx = canvas.getContext('2d');
+
+ctx.font= "bold italic 40px Agency FB";
+ctx.filltext("Hola mundo", 20,100);
+ctx.font= "bold italic 40px Agency FB";
+ctx.filltext("Hola mundo", 20,300);
