b3c5d44917fcd9b94bfb5b87bf343b1f18fad8ed
srMOY240 <elmoy240@gmail.com>
2022-09-15 09:58:21 -0600

    practica4
    inicio unidad 4 con less

---
+++ unidad4/clase1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public\css\main.css">
+</head>
+<body>
+    <div class="contaner" >
+        <form action="">
+            <fieldset>
+                <legend>
+                    Datos de acceso
+                </legend>
+
+                <label for="">email</label>
+                <input type="text" placeholder="write here">
+
+                <label for="">password</label>
+                <input type="text" placeholder="********">
+
+                <button type="submit">
+                    acceder
+                </button>
+            </fieldset>
+        </form>
+
+    </div>
+</body>
+</html>
