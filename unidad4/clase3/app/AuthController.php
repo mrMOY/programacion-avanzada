7001822d63d73ada7791a4b950f2fca56da183eb
srMOY240 <elmoy240@gmail.com>
2022-09-27 10:08:45 -0600

    practica 8
    Implementa el login utilizando la api a través de un controlador con php

---
+++ unidad4/clase3/app/AuthController.php
@@ -0,0 +1,49 @@
+<?php 
+
+    if (isset($_POST['action'])) {
+        switch ($_POST['action']) {
+            case 'access':
+                    $AuthController = new AuthController();
+
+                    $email = strip_tags($_POST['email']);
+                    $password = strip_tags($_POST['email']);
+                break;
+            
+        }
+    }
+    class AuthController{
+        public function login($email,$password)
+        {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => 'moisesm_19@alu.uabcs.mx','password' => '4%7@QcV9uK2nAs'),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            $response = json_encode($response);
+            if (isset($response->code) && $response->code > 0) {
+                session_start();
+                $_SESSION['name']= $response->data->name;
+
+                $_SESSION['name'] = $response->data->name;
+                header("location:./../products/index.php");
+
+            }else {
+                header("location:../?error=true") ;          
+             }
+        }
+    }
+
+
+?>
--- unidad4/clase3/layouts/head.template.php
+++ unidad4/clase3/layouts/head.template.php
@@ -3,7 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     <style media="screen">
             aside{
