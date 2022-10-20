e5f89e5ba7fe6a9bcc522ec147a172a7da854d5a
srMOY240 <elmoy240@gmail.com>
2022-10-20 09:52:08 -0600

    9.- Implementa un seeder para insertar un usuario

--- unudad5-laravel/reservaciones/app/Http/Controllers/UserController.php
+++ unudad5-laravel/reservaciones/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -13,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        echo "hola";
+        return User::all();
     }
 
     /**
